49b38ce8669987e0f4eacfb078ef38ec88b4340c
brothersjuniors <101852740+brothersjuniors@users.noreply.github.com>
2023-02-19 14:43:41 +0900

    Add files via upload

---
+++ FindAsiaApp/FindAsiaApp/Views/DetailCountryView.swift
@@ -0,0 +1,39 @@
+
+
+import UIKit
+
+class DetailCountryView: UIViewController {
+    
+    @IBOutlet weak var countryLabel: UILabel!
+    var selectedCountry = "Country"
+    
+    @IBOutlet weak var countryImage: UIImageView!
+    var selectedImage: UIImage?
+
+    @IBOutlet weak var historyTextView: UITextView!
+    var selectedHistory = ""
+    @IBOutlet weak var citizenNumberLabel: UILabel!
+    var selectedNumber = ""
+    @IBOutlet weak var squeraLabel: UILabel!
+    var selectedSqure = ""
+    @IBOutlet weak var tipTextView: UITextView!
+    var selectedTips = ""
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.title = ""
+        countryLabel.text = selectedCountry
+        navigationItem.title = selectedCountry
+        historyTextView.text = selectedHistory
+        tipTextView.text = selectedTips
+        countryImage.image = selectedImage
+        citizenNumberLabel.text = String(selectedNumber)
+        squeraLabel.text = selectedSqure
+
+        
+        
+    }
+    
+    
+}
---
+++ FindAsiaApp/FindAsiaApp/Views/ViewController.swift
@@ -0,0 +1,115 @@
+//
+//  ViewController.swift
+//  FindAsiaApp
+//
+//  Created by 近江伸一 on 2023/02/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    var selectCountry = ""
+    var selectedNum: [Int] = [0]
+    var selecnumber = 0
+    var article = Article()
+    @IBOutlet weak var selectButton: UIButton!
+    let countrys = [
+        "Asia","China","Korea","Japan"]
+    let screenWidth = UIScreen.main.bounds.width
+    let screenHeight = UIScreen.main.bounds.height
+    let images = UIImageView()
+    
+    @IBOutlet weak var coutryPicker: UIPickerView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        coutryPicker.delegate = self
+        
+        
+        navigationItem.title = "Asia"
+        navigationItem.largeTitleDisplayMode = .automatic
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        images.image = UIImage(named: "1")
+        images.layer.cornerRadius = 100
+        images.frame = CGRect(x:0, y:0, width:300, height:300)
+        
+        images.center = CGPoint(x:screenWidth/2, y:screenHeight * 6/15)
+        
+        self.view.addSubview(images)
+        
+    }
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return countrys.count
+    }
+    func pickerView(_ pickerView: UIPickerView,
+                    titleForRow row: Int,
+                    forComponent component: Int) -> String? {
+        
+        return countrys[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView,
+                    didSelectRow row: Int,
+                    inComponent component: Int) {
+        
+        navigationItem.title = countrys[row]
+        
+        
+        selectCountry = countrys[row]
+        
+        if selectCountry == countrys[0] {
+            selectButton.isEnabled = false
+        } else if selectCountry == countrys[1] {
+            selectButton.isEnabled = true
+            selecnumber = 1
+        } else {
+            selectButton.isEnabled = true
+            selecnumber = 2
+            
+            
+        }
+        
+    }
+    
+    @IBAction func findButton(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "DetailCountryView", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: "DetailCountryView") as! DetailCountryView
+        //
+        if selectCountry == countrys[0] {
+            print("nothing")
+        } else if selectCountry == countrys[1] {
+            vc.selectedCountry = article.country[0].name
+            vc.selectedTips = article.country[0].article
+            vc.selectedImage = UIImage(named: String(article.country[0].image))
+            vc.selectedNumber = String(article.country[0].number)
+            vc.selectedSqure = article.country[0].scale
+            vc.selectedHistory = article.country[0].history
+            self.navigationController?.pushViewController(vc, animated: true)
+            
+        }  else if selectCountry == countrys[2]{
+            vc.selectedCountry = article.country[1].name
+            vc.selectedTips = article.country[1].article
+            vc.selectedImage = UIImage(named: String(article.country[1].image))
+            vc.selectedNumber = String(article.country[1].number)
+            vc.selectedSqure = article.country[1].scale
+            vc.selectedHistory = article.country[1].history
+            self.navigationController?.pushViewController(vc, animated: true)
+        }  else if selectCountry == countrys[3]{
+            vc.selectedCountry = article.country[2].name
+            vc.selectedTips = article.country[2].article
+            vc.selectedImage = UIImage(named: String(article.country[2].image))
+            vc.selectedNumber = String(article.country[2].number)
+            vc.selectedSqure = article.country[2].scale
+            vc.selectedHistory = article.country[2].history
+            self.navigationController?.pushViewController(vc, animated: true)
+            
+            
+        }
+    }
+}
+
